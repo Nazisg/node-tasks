546e53aab8b8348693d9b97b02ee6570cb52a7bb
Nazisg <isgnezrin@gmail.com>
2024-05-27 21:14:42 +0400

    port-ejs-webpack

--- ejs-webpack/Port/src/creators/heroCreators.js
+++ ejs-webpack/Port/src/creators/heroCreators.js
@@ -1,16 +1,5 @@
-import Typed from "typed.js";
-const hello = document.querySelector(".hello")
-const paragraph = document.querySelector(".paragraph")
-const btn = document.querySelector(".btn")
+const title = document.querySelector(".title")
 
 export const bindHeroPart = (data) => {
-    hello.innerHTML = data.hello
-    paragraph.innerHTML = data.description
-    btn.innerHTML = data.btnTxt
-
-    const typed = new Typed(".cd-words-wrapper", {
-        strings: data.typedWords,
-        loop: true,
-        typeSpeed: 50
-    });
+    title.innerHTML = data.title
 }
